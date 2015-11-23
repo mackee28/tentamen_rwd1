8c23a08506a211317df29986e4c9be82b78877e6
mackee28 <markus_r@icloud.com>
2015-11-23 10:13:10 +0100

    remove index.jade and add gulp + package

--- gulpfile.js
+++ gulpfile.js
@@ -6,71 +6,100 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 // Requiring Sourcemaps
 var sourcemaps = require('gulp-sourcemaps');
-// Auto refresh browser on file save
+//Auto refresh browser on file save
 var browserSync = require('browser-sync');
 // Require merge-stream to output multilple tasks to multiple destinations
 var merge = require('merge-stream');
-// Require gulp-jade
-var jade = require('gulp-jade');
-
+//Require Nunjucks Template Engine
+var nunjucksRender = require('gulp-nunjucks-render');
+// Plumber, Error handling
+var plumber = require('gulp-plumber');
+// Other requires
+var notify = require('gulp-notify');
+//Read Data
+var data = require('gulp-data');
+//read JSON data
+var fs = require('fs');
 
 // Start browserSync server
-gulp.task('browserSync', function () {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        }
-    });
-});
-
-// SASS
-gulp.task('sass', function () {
-    return gulp.src('app/scss/**/*.+(scss|sass)') // Gets all files ending with .scss or .sass in app/scss
-        .pipe(sourcemaps.init()) // Initialize sourcemap plugin
-        .pipe(sass()) // Initialize sass
-        .pipe(autoprefixer()) // Passes it through gulp-autoprefixer 
-        .pipe(sourcemaps.write()) // Writing sourcemaps
-        .pipe(gulp.dest('app/css'))
-        // Reloading the stream
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
-
-// JADE
-gulp.task('jade', function () {
-    gulp.src('app/jade/*.jade')
-        .pipe(jade({
-            pretty: true})) // pip to jade plugin
-        .pipe(gulp.dest('app/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
 });
 
 
-// WATCH 'gulp'
-gulp.task('watch', ['browserSync'], function() {
-  gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
-  gulp.watch('app/jade/**', ['jade']);
-  gulp.watch('app/index.html', browserSync.reload);
+function customPlumber(errTitle) {
+  return plumber({
+    errorHandler: notify.onError({
+      // Customizing error title
+      title: errTitle || "Error running Gulp",
+      message: "Error: <%= error.message %>",
+        sound: "Submarine"
+    })
+  });
+}
+
+gulp.task('nunjucks', function() {
+  nunjucksRender.nunjucks.configure(['app/templates/'], {watch: false});
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+    .pipe(customPlumber('Error Running Nunjucks'))
+    .pipe(data(function() {
+      return JSON.parse(fs.readFileSync('./app/data.json'))
+    }))
+    .pipe(nunjucksRender())
+    .pipe(gulp.dest('app'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('prod', function () {
-    var html = gulp.src('app/*.html')
-        .pipe(gulp.dest('dist'));
 
-    var css = gulp.src('app/css/*.css')
-        .pipe(gulp.dest('dist/css'));
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.+(scss|sass)') // Gets all files ending with .scss or .sass in app/scss
+    .pipe(sourcemaps.init()) // Initialize sourcemap plugin
+// Replacing plumber with customPlumber
+    .pipe(customPlumber('Error Running Sass'))
+    .pipe(sass()) // Initialize sass
+    .pipe(autoprefixer()) // Passes it through gulp-autoprefixer 
+    .pipe(sourcemaps.write()) // Writing sourcemaps
+    .pipe(gulp.dest('app/css'))
+    // Reloading the stream
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
-    var img = gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
-        .pipe(gulp.dest('dist/images'));
 
-    var js = gulp.src('app/js/*.js')
-        .pipe(gulp.dest('dist/js'));
+gulp.task('watch', ['browserSync', 'nunjucks', 'sass'], function() {
+gulp.watch([
+    'app/templates/**/*',
+    'app/pages/**/*.+(html|nunjucks)',
+    'app/data.json',
+    ], ['nunjucks'] // runs Nunjucks task
+  );
+gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
+gulp.watch('app/index.html', browserSync.reload);
+});
 
-    return merge(html, css, img, js);
+
+gulp.task('prod', function(){
+ var html=gulp.src('app/*.html')
+.pipe(gulp.dest('dist'))
+ 
+ var css=gulp.src('app/css/*.css')
+.pipe(gulp.dest('dist/css'))
+ 
+ var img=gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+.pipe(gulp.dest('dist/images'))
+ 
+ var js=gulp.src('app/js/*.js')
+.pipe(gulp.dest('dist/js'))
+ 
+ return merge(html, css, img, js);
 });
 
 // Creating a default task
-gulp.task('default', ['sass', 'jade', 'watch', 'prod']);
+gulp.task('default', ['sass', 'watch', 'prod']);
